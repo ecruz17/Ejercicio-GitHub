926ce6ad944c95f761061b6873c4dddbf3c4418a
ecruz17 <ecruz17@ucol.mx>
2020-10-26 12:46:16 -0600

    added comillas

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let a = 5;
 let b = 6;
 let c = a + b;
 
-console.log(`${a} + ${b} = ${c}`)
+console.log(`${a} + ${b} = ${c}`);
 
-console.log(`${a} - ${b} = ${a - b}`)
+console.log(`${a} - ${b} = ${a - b}`);
